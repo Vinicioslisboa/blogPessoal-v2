95face7f70fd7cb94d3bb58eeb13e4be35effc27
Vinícios Lisboa <developerlisboa@gmail.com>
2021-10-05 16:41:07 -0300

    Alterações de rotas

--- src/app/delete/tema-delete/tema-delete.component.ts
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -38,7 +38,8 @@ export class TemaDeleteComponent implements OnInit {
   apagar(){
     this.temaService.deleteTema(this.idTema).subscribe(() => {
       alert('Tema apagado com sucesso!')
-      this.router.navigate(['/temas'])
+      
+      this.router.navigate(['/tema'])  
     })
   }
 
