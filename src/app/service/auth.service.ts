e584ae9df852b3747a65edc61d5aa17f4cc9e694
Vinícios Lisboa <developerlisboa@gmail.com>
2021-10-06 15:48:44 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
     return this.http.post<User>('https://blogdolisboa.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogdolisboa.herokuapp.com/usuarios/${id}`)
+  }
+
   logado(){
     let ok = false
 
